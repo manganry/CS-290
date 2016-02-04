d40c8a48fce5068595902b1c4c36cefdd189260b
manganry <rmangan3@gmail.com>
2016-02-04 12:38:31 -0800

    Update script.js
    Added event listeners to buttons

--- script.js
+++ script.js
@@ -15,8 +15,12 @@ function buildTable(){
 		var row = document.createElement("tr");
 		for (var k = 1; k <= columnCount; k++){
 			var cell = document.createElement("td");
-			cell.textContent = k + ", " + j;
+      var cellLabel = k + "," + j;
+			cell.textContent = cellLabel;
+      cell.style.border = "1px solid black"
 			cell.style.textAlign = "center";
+      cell.setAttribute("class", "rowCell");
+      cell.setAttribute("id", cellLabel);
 			row.appendChild(cell);
 		}
 		table.appendChild(row);
@@ -24,16 +28,95 @@ function buildTable(){
 	
 	return table;
 }
-document.body.appendChild(buildTable());
+var myTable = document.body.appendChild(buildTable());
+document.getElementById("1,1").style.border = "3px solid black";
+var currentRow = 1;
+var currentCol = 1;
+var topRow = 1;
+var bottomRow = 3;
+var leftCol = 1;
+var rightCol = 4;
+
 
 
 function addButton(name){
 	var button = document.createElement("button");
 	button.textContent = name;
+  //button.setAttribute("class", "myButtons");
 	return button;
 }
-document.body.appendChild(addButton("Up"));
-document.body.appendChild(addButton("Down"));
-document.body.appendChild(addButton("Left"));
-document.body.appendChild(addButton("Right"));
-document.body.appendChild(addButton("Mark Cell"));
+var upButton = document.body.appendChild(addButton("Up"));
+var downButton = document.body.appendChild(addButton("Down"));
+var leftButton = document.body.appendChild(addButton("Left"));
+var rightButton = document.body.appendChild(addButton("Right"));
+var markButton = document.body.appendChild(addButton("Mark Cell"));
+
+
+upButton.addEventListener("click", function(event){
+  var currentCellId = String(currentCol) + "," + String(currentRow);
+  var currentCell = document.getElementById(currentCellId);
+  if (currentRow > 1){
+  	currentCell.style.border = "1px solid black";
+  
+  	currentRow -= 1;
+  
+    currentCellId = String(currentCol) + "," + String(currentRow);
+    currentCell = document.getElementById(currentCellId);
+    currentCell.style.border = "3px solid black";
+  }
+});
+
+
+downButton.addEventListener("click", function(event){
+  var currentCellId = String(currentCol) + "," + String(currentRow);
+  var currentCell = document.getElementById(currentCellId);
+  if (currentRow < 3){
+    currentCell.style.border = "1px solid black";
+
+    currentRow += 1;
+
+    currentCellId = String(currentCol) + "," + String(currentRow);
+    currentCell = document.getElementById(currentCellId);
+    currentCell.style.border = "3px solid black";
+  }
+});
+
+
+leftButton.addEventListener("click", function(event){
+  var currentCellId = String(currentCol) + "," + String(currentRow);
+  var currentCell = document.getElementById(currentCellId);
+  if (currentCol > 1){
+    currentCell.style.border = "1px solid black";
+
+    currentCol -= 1;
+
+    currentCellId = String(currentCol) + "," + String(currentRow);
+    currentCell = document.getElementById(currentCellId);
+    currentCell.style.border = "3px solid black";
+  }
+});
+
+
+rightButton.addEventListener("click", function(event){
+  var currentCellId = String(currentCol) + "," + String(currentRow);
+  var currentCell = document.getElementById(currentCellId);
+  if (currentCol < 4){
+    currentCell.style.border = "1px solid black";
+
+    currentCol += 1;
+
+    currentCellId = String(currentCol) + "," + String(currentRow);
+    currentCell = document.getElementById(currentCellId);
+    currentCell.style.border = "3px solid black";
+  }
+});
+
+
+var cellList = document.getElementsByClassName("rowCell");
+markButton.addEventListener("click", function(event) {
+  for (var i = 0; i < cellList.length; i++){
+    if (cellList[i].style.border == "3px solid black"){
+    	cellList[i].style.backgroundColor = "yellow";
+    }
+  }
+});
